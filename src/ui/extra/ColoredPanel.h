a6d5b927422fa12ac2273aa1399b20eb74658fc6
Cedric Niyikiza <cedric.niyikiza@student.vinci.be>
2025-07-12 16:41:05 +0200

    update - modify style of basic component coloredPanel

--- src/ui/extra/ColoredPanel.h
+++ src/ui/extra/ColoredPanel.h
@@ -3,20 +3,33 @@
 #include <JuceHeader.h>
 
 //==============================================================================
-// Classe simple pour les panneaux colorés
+// Classe simple pour les panneaux colorés avec shadow performant
 class ColoredPanel : public juce::Component
 {
 public:
-    ColoredPanel(juce::Colour color) : panelColor(color) 
-    {
-        setOpaque(true);
+    ColoredPanel(juce::Colour color) 
+        : panelColor(color)    {
+        setOpaque(false);
     }
     
     void paint(juce::Graphics& g) override
     {
-        g.fillAll(panelColor);
+        auto bounds = getLocalBounds().toFloat();
+        
+        // Créer le path pour le rectangle arrondi
+        juce::Path panelPath;
+        panelPath.addRoundedRectangle(bounds, 10.0f);
+
+        // Rendre le panel
+        g.setColour(panelColor);
+        g.fillPath(panelPath);
+    }
+    
+    void resized() override
+    {
     }
     
 private:
     juce::Colour panelColor;
+
 }; 
