f969e15ae4402e9bd102c98386401835bfaf164b
Cedric Niyikiza <cedric.niyikiza@student.vinci.be>
2025-07-24 03:47:13 +0200

    refactor - update Modulation

--- src/model/Modulation.cpp
+++ src/model/Modulation.cpp
@@ -1,44 +1,66 @@
 #include "Modulation.h"
 
-// Constructeurs
-Modulation::Modulation() : modulationType(0), fromChordIndex(0), toChordIndex(0) {}
+Modulation::Modulation() 
+    : modulationType(0), fromChordIndex(-1), toChordIndex(-1) {
+}
 
 Modulation::Modulation(int type, int fromChordIndex, int toChordIndex) 
-    : modulationType(type), fromChordIndex(fromChordIndex), toChordIndex(toChordIndex) {}
+    : modulationType(type), fromChordIndex(fromChordIndex), toChordIndex(toChordIndex) {
+}
 
-// Setters
 void Modulation::setModulationType(int newType) {
-    if (modulationType != newType) {
+    if (newType >= 0 && newType <= 3) {  // 0-3 pour les 4 types de modulation
         modulationType = newType;
         notifyChange();
     }
 }
 
 void Modulation::setFromChordIndex(int newFromChordIndex) {
-    if (fromChordIndex != newFromChordIndex) {
-        fromChordIndex = newFromChordIndex;
-        notifyChange();
-    }
+    fromChordIndex = newFromChordIndex;
+    notifyChange();
 }
 
 void Modulation::setToChordIndex(int newToChordIndex) {
-    if (toChordIndex != newToChordIndex) {
-        toChordIndex = newToChordIndex;
-        notifyChange();
-    }
+    toChordIndex = newToChordIndex;
+    notifyChange();
 }
 
-// Méthodes utilitaires
 juce::String Modulation::toString() const {
-    juce::String result = "Modulation(";
-    result += "Type:" + juce::String(modulationType);
-    result += ", From:" + juce::String(fromChordIndex);
-    result += ", To:" + juce::String(toChordIndex);
+    juce::String result;
+    
+    // Types de modulation
+    const char* modulationTypes[] = {
+        "Perfect Cadence", 
+        "Pivot Chord", 
+        "Alteration", 
+        "Chromatic"
+    };
+    
+    if (modulationType >= 0 && modulationType <= 3) {
+        result = modulationTypes[modulationType];
+    } else {
+        result = "Unknown";
+    }
+    
+    result += " (";
+    
+    if (fromChordIndex >= 0) {
+        result += "from chord " + juce::String(fromChordIndex);
+    } else {
+        result += "from unknown";
+    }
+    
+    if (toChordIndex >= 0) {
+        result += " to chord " + juce::String(toChordIndex);
+    } else {
+        result += " to unknown";
+    }
+    
     result += ")";
+    
     return result;
 }
 
-// Méthodes privées
 void Modulation::notifyChange() {
     if (onModulationChanged) {
         onModulationChanged();
