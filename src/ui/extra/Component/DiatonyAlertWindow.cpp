90f8888fb03afc79cb37a4c2a3f20d3d799532a9
Cedric Niyikiza <cedric.niyikiza@student.vinci.be>
2026-01-14 22:37:25 +0100

    fix - delete comments regarding fix of DiatonyAlertWindow

--- src/ui/extra/Component/DiatonyAlertWindow.cpp
+++ src/ui/extra/Component/DiatonyAlertWindow.cpp
@@ -31,15 +31,12 @@ DiatonyAlertWindow::DiatonyAlertWindow(AlertType type,
     );
     
     okButton->onClick = [this]() {
-        // Récupérer le parent AVANT d'appeler onClose, car celui-ci peut détruire this
         auto* parent = findParentComponentOfClass<juce::DialogWindow>();
         juce::Component::SafePointer<juce::DialogWindow> safeParent(parent);
         
-        // Copier le callback car this peut être détruit après onClose
         auto callback = onClose;
         if (callback) callback();
         
-        // Vérifier que le parent existe toujours avant d'appeler exitModalState
         if (safeParent != nullptr)
             safeParent->exitModalState(0);
     };
